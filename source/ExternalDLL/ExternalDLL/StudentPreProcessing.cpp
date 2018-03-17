2dd2dda108368985a992740d095cb87c47a25ff0
Draivun <nick_goris@outlook.com>
2018-03-17 15:00:33 +0100

    First try at greyscaling

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -2,7 +2,21 @@
 
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-    return nullptr; // Daadwerkelijke implementatie Greyscaling
+    //return nullptr; // Daadwerkelijke implementatie Greyscaling
+    int source_width = image.getWidth();
+    int source_height = image.getHeight();
+
+    IntensityImage * greyscaled = ImageFactory::newIntensityImage(source_width, source_height);
+
+    for (int new_width = 0; new_width < source_width; ++new_width) {
+        for (int new_height = 0; new_height < source_height; ++new_height) {
+            RGB source_pixel = image.getPixel(new_width, new_height);
+            Intensity new_pixel = source_pixel.b;
+
+            greyscaled->setPixel(new_width, new_height, new_pixel);
+        }
+    }
+    return greyscaled;
 
 }
 
